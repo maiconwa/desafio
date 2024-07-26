cfd137846b6b6b18eb98c644ffed986fb37dfd03
maicon william <maicon.w.a@gmail.com>
2024-07-25 22:44:28 -0300

    remove logger.py

--- myapp/logger.py
+++
@@ -1,7 +0,0 @@
-import logging
-logger = logging.getLogger(__name__)
-
-
-def log_view(request):
-    logger.info('User IP: %s Request Method: %s Request URL: %s' % (request.META.get('REMOTE_ADDR'), request.method, request.path))
-    # Codes
