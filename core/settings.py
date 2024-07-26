97ebc4474779ae19f534553437670ae2e4977093
maicon william <maicon.w.a@gmail.com>
2024-07-26 01:51:16 -0300

    setup readme

--- core/settings.py
+++ core/settings.py
@@ -110,18 +110,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': os.environ["ENGINE"],
-        'NAME': os.environ["NAME"],
-        'USER': os.environ["USER"],
-        'PASSWORD': os.environ["PASSWORD"],
-        'HOST': os.environ["HOST"],
-        'PORT': os.environ["PORT"],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': os.environ["ENGINE"],
+    #     'NAME': os.environ["NAME"],
+    #     'USER': os.environ["USER"],
+    #     'PASSWORD': os.environ["PASSWORD"],
+    #     'HOST': os.environ["HOST"],
+    #     'PORT': os.environ["PORT"],
+    # }
 }
 
 
